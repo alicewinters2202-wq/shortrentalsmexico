31452ce13b6946051de6406228a44b3135e3374d
unknown <Alicewinters2202@gmail.com>
2026-04-02 21:05:22 -0600

    Eliminar Swagger de main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,34 +4,14 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import * as path from 'path';
 
-// 👇 IMPORTS NUEVOS PARA SWAGGER
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
   app.enableCors({ origin: '*' });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-
   app.setGlobalPrefix('api');
-
   app.useStaticAssets(path.join(__dirname, '..', 'imagenes'), {
     prefix: '/imagenes',
   });
-
-  // 👇 SWAGGER CONFIG
-  const config = new DocumentBuilder()
-    .setTitle('ShortStayMX API')
-    .setDescription('API docs for properties, availability, cities, etc.')
-    .setVersion('1.0')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-
-  // 👉 Ruta:  https://tu-backend.com/api/docs
-  SwaggerModule.setup('api/docs', app, document);
-
   await app.listen(process.env.PORT ?? 3001);
 }
-
 bootstrap();
